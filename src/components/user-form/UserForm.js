900c429736fa94738d59e553d09734f88bcae5f5
mvximenko <mr.mxmnk@gmail.com>
2021-10-12 16:55:32 +0900

    Redesign UserForm

--- src/components/user-form/UserForm.js
+++ src/components/user-form/UserForm.js
@@ -1,22 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { useDispatch, shallowEqual } from 'react-redux';
 import { useSelector } from '../../redux/store';
 import { getUser, resetUser, updateUser } from '../../redux/slices/userSlice';
 import api from '../../utils/api';
 import {
-  Container,
+  OuterContainer,
+  InnerContainer,
+  Form,
   Heading,
   Grid,
-  Input,
-  InputSubmit,
-  Span,
   Wrapper,
+  Input,
+  Buttons,
+  Button,
 } from './UserFormStyles';
 
 export default function UserForm() {
   const dispatch = useDispatch();
-  const [isOpen, setIsOpen] = useState(false);
   const id = useSelector((state) => state.auth.user._id);
   const user = useSelector((state) => state.user.user, shallowEqual);
   const { name, surname, patronymic, password } = user;
@@ -47,47 +48,68 @@ export default function UserForm() {
   };
 
   return (
-    <Container>
-      <Heading>Edit Profile</Heading>
-      <form onSubmit={handleSubmit}>
-        <Grid>
-          <Input
-            type='text'
-            name='name'
-            placeholder='Name'
-            value={name}
-            onChange={onChange}
-          />
-          <Input
-            type='text'
-            name='surname'
-            placeholder='Surname'
-            value={surname}
-            onChange={onChange}
-          />
-          <Input
-            type='text'
-            name='patronymic'
-            placeholder='Patronymic'
-            value={patronymic}
-            onChange={onChange}
-          />
-          {!isOpen ? (
-            <Span onClick={() => setIsOpen(true)}>Change Password?</Span>
-          ) : (
-            <Input
-              type='password'
-              name='password'
-              placeholder='Password'
-              value={password}
-              onChange={onChange}
-            />
-          )}
-        </Grid>
-        <Wrapper>
-          <InputSubmit type='submit' value='Edit Profile' />
-        </Wrapper>
-      </form>
-    </Container>
+    <OuterContainer>
+      <InnerContainer>
+        <Form onSubmit={handleSubmit}>
+          <Heading>Edit Profile</Heading>
+
+          <Grid>
+            <Wrapper>
+              <label htmlFor='name'>Name</label>
+              <Input
+                type='text'
+                name='name'
+                id='name'
+                placeholder='Name'
+                value={name}
+                onChange={onChange}
+              />
+            </Wrapper>
+
+            <Wrapper>
+              <label htmlFor='surname'>Surname</label>
+              <Input
+                type='text'
+                name='surname'
+                id='surname'
+                placeholder='Surname'
+                value={surname}
+                onChange={onChange}
+              />
+            </Wrapper>
+
+            <Wrapper>
+              <label htmlFor='patronymic'>Patronymic</label>
+              <Input
+                type='text'
+                name='patronymic'
+                id='patronymic'
+                placeholder='Patronymic'
+                value={patronymic}
+                onChange={onChange}
+              />
+            </Wrapper>
+
+            <Wrapper>
+              <label htmlFor='password'>Password</label>
+              <Input
+                type='password'
+                name='password'
+                id='password'
+                placeholder='Password'
+                value={password}
+                onChange={onChange}
+              />
+            </Wrapper>
+
+            <Buttons>
+              <Button type='submit' variant='blue'>
+                Edit Profile
+              </Button>
+            </Buttons>
+          </Grid>
+        </Form>
+      </InnerContainer>
+    </OuterContainer>
   );
 }
